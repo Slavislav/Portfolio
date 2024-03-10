eadb8ba0fd77b06a96109b95f76d6594c5131b6b
Slav Office pc <slavastinov@gmail.com>
2024-03-10 15:22:15 +0200

    changed the color style and image function added

--- js/script.js
+++ js/script.js
@@ -1,9 +1,23 @@
-// Function to toggle between light and dark mode
+// Function to toggle between light and dark mode and border add over the image
 function toggleMode() {
   const body = document.body;
   body.classList.toggle("light-mode");
   body.classList.toggle("dark-mode");
+
+  const image = document.querySelector('.about-me-image img');
+  if (image) {
+  image.classList.toggle("image-border");
 }
+}
+
+// Function to handle click event on the image
+function handleImageClick() {
+  const image = document.querySelector(".about-me-image img");
+  if (image) {
+    image.classList.toggle("image-clicked"); // Toggle the class for click effect
+  }
+}
+
 
 // Event listener for the mode toggle button
 const toggleButton = document.getElementById("toggle-mode");
@@ -11,3 +25,7 @@ if (toggleButton) {
   toggleButton.addEventListener("click", toggleMode);
 }
 
+const imageElement = document.querySelector(".about-me-image img");
+if (imageElement) {
+  imageElement.addEventListener("click", handleImageClick);
+}
